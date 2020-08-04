ae5ec1344126b073d8328b8338c1392a8f986144
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-04 22:48:49 +0400

    Seperating out Data Layer

--- BeerShop.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BeerShop.DataAccess
-{
-    public class Class1
-    {
-    }
-}
--- BeerShop.Utility/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BeerShop.Utility
-{
-    public class Class1
-    {
-    }
-}
