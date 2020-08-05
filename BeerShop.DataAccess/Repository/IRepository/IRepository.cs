4da64d5bb4d1454df9571fdecd6fcea7ee664dae
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-05 22:04:08 +0400

    Add Repository Interface

---
+++ BeerShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BeerShop.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T: class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedEnumerable<T>> orderBy = null,
+            string IncludePoreperties = null
+            );
+
+        T GetFirstOrDefault(
+
+            Expression<Func<T, bool>> filter = null,            
+            string IncludePoreperties = null
+            );
+
+        void Add(T entity); 
+
+        void Remove(int id);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
