a895f92c6178c5b6e8232157a22b07a1549b4311
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-06 22:28:30 +0400

    Add Style Repository and Interface

---
+++ BeerShop.DataAccess/Repository/StyleRepository.cs
@@ -0,0 +1,34 @@
+﻿using BeerShop.DataAccess.Data;
+using BeerShop.DataAccess.Repository.IRepository;
+using BeerShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeerShop.DataAccess.Repository
+{
+    class StyleRepository : Repository<Style> , IStyleRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StyleRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Style style)
+        {
+            var objFromDb = _db.Styles.FirstOrDefault(s => s.Id == style.Id);
+
+            if (objFromDb != null)
+            {
+                objFromDb.Name = style.Name;
+
+                _db.SaveChanges();
+            }
+            
+
+        }
+    }
+}
