e93f90986cfd41ba20c39e087b8492dee8407ad8
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-12 14:15:13 +0400

    Create Style Controller And Style Index and Get All Actions

---
+++ BeerShop/Areas/Admin/Controllers/StyleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BeerShop.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerShop.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class StyleController : Controller
+    {
+        private readonly IUnitOfWork _unitOfwork;
+
+        public StyleController(IUnitOfWork unitOfWork)
+        {
+            _unitOfwork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfwork.Style.GetAll();
+            return Json(new { data = allObj });
+        }
+
+
+        #endregion
+    }
+}
