518d9b83b167f10a4f54bf789ec26a980cb201e4
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-14 16:30:22 +0400

    Add Product Controller

---
+++ BeerShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BeerShop.DataAccess.Repository.IRepository;
+using BeerShop.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace BeerShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfwork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfwork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UpSert(int? id)
+        {
+            Product product = new Product();
+            
+            if (id==null)
+            {
+                //Create Product
+                return View(product);
+            }
+            // Edit Product
+            product = _unitOfwork.Product.Get(id.GetValueOrDefault());
+            if (product==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpSert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.Id == 0 )
+                {
+                    _unitOfwork.Product.Add(product);                    
+                }
+                else
+                {
+                    _unitOfwork.Product.Update(product);                    
+                }
+                _unitOfwork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfwork.Product.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfwork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error While Deleting" });
+            }
+            _unitOfwork.Product.Remove(objFromDb);
+            _unitOfwork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
