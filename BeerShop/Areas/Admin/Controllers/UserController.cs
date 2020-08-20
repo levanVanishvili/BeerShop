9e5ddfd3ef101fdfc311cd3e0ce58d51e2cd01ef
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-20 15:26:59 +0400

    Add  User Controller

---
+++ BeerShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BeerShop.DataAccess.Data;
+using BeerShop.DataAccess.Repository.IRepository;
+using BeerShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using Microsoft.EntityFrameworkCore
+
+namespace BeerShop.Areas.Admin.Controllers
+{
+    //different way: create functions(using ApplicationDbContext) without using repository pattern  
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if (user.Company==null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+            return Json(new { data = userList });
+        }
+        
+
+        #endregion
+    }
+}
