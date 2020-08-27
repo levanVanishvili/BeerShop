8e579eb1bbfa296fa4279e2104cbab4c79b07946
levani vanishvili <levan.vanishvili@gmail.com>
2020-08-27 17:25:40 +0400

    Shopping Cart Index Action Get

---
+++ BeerShop/Areas/Customer/Controllers/CartController1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BeerShop.DataAccess.Repository.IRepository;
+using BeerShop.Models.ViewModels;
+using BeerShop.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BeerShop.Areas.Customer.Controllers
+{
+    public class CartController1 : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController1(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //retrieving shopping cart from database
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u=>u.ApplicationUserId==claim.Value, includePoreperties:"Product")
+            };
+            //get applicationuser with company
+            ShoppingCartVM.OrderHeader.OrderTotal = 0;
+            ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value, includePoreperties: "Company");
+
+            //iterating through all of the items to calculate the price based on the count 
+            foreach (var list in ShoppingCartVM.ListCart)
+            {
+                list.Price = SD.GetPriceBasedOnQuantity(list.Count, list.Product.Price, list.Product.Price50, list.Product.Price100);
+
+                ShoppingCartVM.OrderHeader.OrderTotal += (list.Price * list.Count);
+                list.Product.Description = SD.ConvertToRawHtml(list.Product.Description);
+                if (list.Product.Description.Length>100)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0,99)+"...";
+                }
+            }
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
