8ba848c52d4b35f110d64d8e03abf7598e07334b
levani vanishvili <levan.vanishvili@gmail.com>
2020-09-07 14:40:25 +0400

    Add Order Controller Index API

---
+++ BeerShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerShop.DataAccess.Repository.IRepository;
+using BeerShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitofWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitofWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetOrderList()
+        {
+            IEnumerable<OrderHeader> orderHeaderList;
+            orderHeaderList = _unitofWork.OrderHeader.GetAll(includePoreperties: "ApplicationUser");
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+    }
+}
