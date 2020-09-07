09eea9143c2f2044492b1917651fef59911b19f8
levani vanishvili <levan.vanishvili@gmail.com>
2020-09-07 12:15:08 +0400

    Add Validations for Summary

--- BeerShop.Models/OrderHeader.cs
+++ BeerShop.Models/OrderHeader.cs
@@ -38,17 +38,17 @@ namespace BeerShop.Models
 
         public string TransactionId { get; set; }
 
-
+        [Required]
         public string PhoneNumber { get; set; }
-
+        [Required]
         public string StreetAddress { get; set; }
-
+        [Required]
         public string City { get; set; }
-
+        [Required]
         public string State { get; set; }
-
+        [Required]
         public string PostalCode { get; set; }
-
+        [Required]
         public string Name { get; set; }
     }
 }
