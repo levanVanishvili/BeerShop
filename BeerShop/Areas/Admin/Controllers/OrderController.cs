00033feb07cbc4f021f5d5cdecd0debed9417f53
levani vanishvili <levan.vanishvili@gmail.com>
2020-09-07 16:03:33 +0400

    Order Controller Modification

--- BeerShop/Areas/Admin/Controllers/OrderController.cs
+++ BeerShop/Areas/Admin/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BeerShop.DataAccess.Repository.IRepository;
 using BeerShop.Models;
+using BeerShop.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,10 +28,40 @@ namespace BeerShop.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetOrderList()
+        public IActionResult GetOrderList(string status)
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
             IEnumerable<OrderHeader> orderHeaderList;
-            orderHeaderList = _unitofWork.OrderHeader.GetAll(includePoreperties: "ApplicationUser");
+
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaderList = _unitofWork.OrderHeader.GetAll(includePoreperties: "ApplicationUser");
+            }
+            else
+            {
+                orderHeaderList = _unitofWork.OrderHeader.GetAll(u=>u.ApplicationUserId==claim.Value, includePoreperties:"ApplicationUser");
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList = orderHeaderList.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusApproved || o.OrderStatus == SD.StatusInProcess || o.OrderStatus == SD.StatusPending); ;
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusShipped);
+                    break;
+                case "rejected":
+                    orderHeaderList = orderHeaderList.Where(o => o.OrderStatus == SD.StatusCancelled || o.OrderStatus == SD.StatusRefunded || o.OrderStatus == SD.PaymentStatusRejected); ;
+                    break;
+                default:
+                    break;
+            }
+
             return Json(new { data = orderHeaderList });
         }
         #endregion
