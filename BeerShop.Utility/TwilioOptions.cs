c2bbd89149f63ca09ea230c399b9eaf338e4e02b
levani vanishvili <levan.vanishvili@gmail.com>
2020-09-09 16:37:50 +0400

    Setting up Twilio for SMS

---
+++ BeerShop.Utility/TwilioOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerShop.Utility
+{
+    public class TwillioOptions
+    {
+        public string PhoneNumber { get; set; }
+        public string AuthToken { get; set; }
+        public string AccountSid { get; set; }
+    }
+}
